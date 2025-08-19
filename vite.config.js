7f9ff7fe159611801a4085b6cdf494ce91bb1b36
Devansh-dev1 <devansh@webnexus.in>
2025-08-19 16:55:28 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,        // ← ye add karo
+    port: 5173,        // optional (default hai)
+    strictPort: true,  // agar port busy ho toh error de
+  }
+});
